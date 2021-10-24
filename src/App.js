c295bc9c2a434675a41a08a73cdd79ff3d09fd2d
jinoo7099 <vxc7099@gmail.com>
2021-10-24 16:21:09 +0900

    22 NavLink 사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import MovieForm from "./components/MovieForm";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -24,11 +26,22 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Movie List</h1>
-      <MovieForm addMovie={addMovie} />
-      {renderMovies}
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/movies">
+          <h1>Movie List</h1>
+          <MovieForm addMovie={addMovie} />
+          {renderMovies}
+        </Route>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+        <Route path="/" exact>
+          <h1>Home</h1>
+        </Route>
+      </div>
+    </Router>
   );
 }
 
