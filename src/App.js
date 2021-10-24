46cc910a55e49001521081576cb09fad1bbe2ae2
jinoo7099 <vxc7099@gmail.com>
2021-10-24 17:44:13 +0900

    27 Home/Movies 페이지 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -4,44 +4,20 @@ import MovieForm from "./components/MovieForm";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Users from "./pages/Users";
-
+import Home from "./pages/Home";
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(
-      movies.filter((movie) => {
-        return movie.id !== id;
-      })
-    );
-  };
-
-  const renderMovies = movies.length
-    ? movies.map((movie) => {
-        return <Movie movie={movie} key={movie.id} removeMovie={removeMovie} />;
-      })
-    : "추가된 영화가 없습니다";
-
-  const addMovie = (movie) => {
-    setMovies([...movies, movie]);
-  };
-
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="container">
           <Switch>
-            <Route path="/movies">
-              <h1>Movie List</h1>
-              <MovieForm addMovie={addMovie} />
-              {renderMovies}
-            </Route>
+            <Route path="/movies"></Route>
             <Route path="/users">
               <Users />
             </Route>
             <Route path="/" exact>
-              <h1>Home</h1>
+              <Home />
             </Route>
           </Switch>
         </div>
